4890d85f2c4faa4300162156e3b733884f56ec34
Razvan <75049110+rax2011@users.noreply.github.com>
2021-11-08 15:03:37 +0200

    Add files via upload

---
+++ 5kyu/Is_my_friend_cheating.java
@@ -0,0 +1,18 @@
+package give_me_a_diamond;
+import java.util.ArrayList;
+import java.util.List;
+public class Is_my_friend_cheating{
+
+
+	 public static List<long[]> removNb(long n) {
+		    List<long[]> result = new ArrayList<long[]>();
+		    long s = n * (n + 1) / 2;
+		    for (long i = 1; i <= n; i++) {
+		      long j = (s - i) / (i + 1);
+		      if (j <= n && i * j == s - i - j) {
+		        result.add(new long[] {i, j});
+		      }
+		    }
+		    return result;
+		  }
+}
